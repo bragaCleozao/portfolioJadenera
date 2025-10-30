b77cc6523e05d36ab442f20ace6491ba0cee3bb1
LeonardoCB <braga.leozao@gmail.com>
2025-10-29 23:58:59 -0300

    Alteração de novas paginas e arquivo js
    Nessa nova versao, foram criadas 2 novas paginas e criado um arquivo de js pra colocar os scripts

---
+++ portfolio/public/assets/js/main.js
@@ -0,0 +1,5 @@
+function redirecionarContato() {
+    window.location.href = "contato.html"; // Coloque a URL da página de destino aqui
+  }
+
+  document.getElementById("btn-contato").addEventListener("click", redirecionarContato);
